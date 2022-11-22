a4eb813dbcb069a4f87b9e29082aa51cc42c5bf5
Erika Sánchez-Femat <erikasafe@gmail.com>
2022-11-21 19:45:32 -0600

    [UPDATE] Added new dependencies and updated the method to interact with NASA API [Author] Erika Sánchez Femat
    -- Modified method to fetch data from NASA

--- src/api_handler.py
+++ src/api_handler.py
@@ -2,20 +2,23 @@ import requests
 from loguru import logger
 
 from consts.consts import API_URL
+from consts.paths import APODPaths
 
-def download_apod_image(path):
+
+def download_apod_image():
     r = requests.get(API_URL)
-    
+    path = APODPaths.image_path
+
     if r.status_code == 200:
         results = r.json()
-        url = results['hdurl']
-        logger.info(f'Fetching APOD image from {url}')
-        
-        if results['media_type'] == 'image':
-            with open(path, 'wb') as f:
+        url = results["hdurl"]
+        logger.info(f"Fetching APOD image from {url}")
+
+        if results["media_type"] == "image":
+            with open(path, "wb") as f:
                 f.write(requests.get(url).content)
-            logger.info(f'Image retrieved successfully. Saved to {path}')
+            logger.info(f"Image retrieved successfully. Saved to {path}")
         else:
-            logger.error('APOD of today is not an image')
+            logger.error("APOD of today is not an image")
     else:
-        logger.error('Could not make the request to NASA. Please try again')
+        logger.error("Could not make the request to NASA. Please try again")
