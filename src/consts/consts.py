52d90d201a92dcea93ec076eab2a2f4b0c2a8b89
Erika Sánchez-Femat <erikasafe@gmail.com>
2022-11-24 19:17:00 -0600

    [UPDATE] Updated code to deploy streamlit app [Author] Erika Sánchez Femat
    -- Updated code to deploy the application to streamlit

--- src/consts/consts.py
+++ src/consts/consts.py
@@ -1,5 +1,3 @@
 API_URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
 IMG_NAME = "original_APOD.jpg"
-RESIZED_NAME = "resized_APOD.jpg"
-OUTPUT_NAME = "color_palette.jpg"
 RESIZE_WIDTH = 500
