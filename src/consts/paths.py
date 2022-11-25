2022beb854e3a428e8279e5c5239f0bd6919d06e
Erika Sánchez Femat <esanchezf1201@alumno.ipn.mx>
2022-11-24 19:19:36 -0600

    [UPDATE] deleted old code [Author] Erika Sánchez Femat
    -- Deleted code that is not used anymore

--- src/consts/paths.py
+++ src/consts/paths.py
@@ -1,6 +1,6 @@
 import os
 
-from consts.consts import IMG_NAME, RESIZED_NAME, OUTPUT_NAME
+from consts.consts import IMG_NAME
 
 PROJECT_PATH = os.path.abspath(os.path.join(__file__, *(os.path.pardir,) * 3))
 DATA_PATH = os.path.join(PROJECT_PATH, "data")
@@ -9,8 +9,3 @@ OUTPUT_PATH = os.path.join(DATA_PATH, "output")
 
 class APODPaths:
     image_path = os.path.join(DATA_PATH, IMG_NAME)
-    resized_img_path = os.path.join(DATA_PATH, RESIZED_NAME)
-
-
-class ColorPalettePaths:
-    output_path = os.path.join(OUTPUT_PATH, OUTPUT_NAME)
